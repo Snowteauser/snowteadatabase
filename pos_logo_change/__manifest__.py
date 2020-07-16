5ce537ee97bc59c5146466de0b00848981692b68
shubhajit <shubhajit.kushari@browseinfo.in>
2020-07-16 21:09:35 +0530

    pos logo change added

---
+++ pos_logo_change/__manifest__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+{
+    "name" : "Odoo Edit/Modify POS Receipt App ",
+    "version" : "13.0.0.0",
+    "category" : "Point of Sale",
+    "price": 9,
+    "currency": 'EUR',
+    "depends" : ['base','sale','point_of_sale'],
+    "author": "BrowseInfo",
+    'summary': 'Change Company Logo in Point of Sale Session and Receipt pos logo change pos receipt edit pos receipt customization pos custom receipt change pos receipt change custom pos receipt edit pos receipt pos logo change pos custom change',
+    "description": """
+            BrowseInfo developed a new odoo/OpenERP module apps
+    odoo pos logo change point of sales logo odoo pos logo on receipt logo change on pos odoo
+    This Module is for odoo Change Company Logo in Point of Sales.
+    odoo Add logo on POS Logo Change on point of sale Logo change on POS Change logo on POS.
+    odoo POS Logo change POS logo on Receipt POS logo on Session odoo
+    odoo Customized POS Receipt POS Customized Receipt Company Name Logo Customer details pos odoo
+    odoo POS Logo on Session and Receipt in odoo POS Receipt and Reprint POS Receipt From Pos Order
+    odoo POS Custom Receipt Customized POS Receipt odoo POS Ticket Logo and Barcode
+    odoo POS Custom Ticket Logo and Barcode custom pos receipt Customizable POS Receipt
+    odoo pos edit pos receipt pos modify pos receipt
+    odoo pos custom pos receipt Odoo POS Order Customization
+    odoo POS Order Customization custom pos order report Pos ticket logo
+    odoo pos Customize POS Receipts pos Receipt Template custom pos order Edit/Modify POS Receipt Edit/Modify
+
+
+    odoo Point of Sale logo change point of sale logo odoo Point of Sale logo on receipt logo change on Point of Sale odoo
+    This Module is for odoo Change Company Logo in Point of Sales.
+    odoo Add logo on Point of Sales Logo Change on POS Logo change on Point of Sale Change logo on Point of Sales.
+    odoo Point of Sale Logo change Point of Sale logo on Receipt Point of Sales logo on Session odoo
+    odoo Customized Point of Sale Receipt Point of Sale Customized Receipt Company Name Logo Customer details Point of Sale odoo
+    odoo Point of Sale Logo on Session and Receipt in Point of Sale Point of Sale Receipt and Reprint Point of Sale Receipt From Point of Sale Order
+    odoo Point of Sales Custom Receipt Customized POS Receipt odoo Point of Sale Ticket Logo and Barcode
+    odoo Point of Sales Custom Ticket Logo and Barcode custom Point of Sale receipt Customizable Point of Sale Receipt
+    odoo Point of Sale edit Point of Sale receipt Point of Sale modify Point of Sale receipt
+    odoo Point of Sales custom Point of Sale receipt Odoo Point of Sale Order Customization
+    odoo Point of Sale Order Customization custom Point of Sale order report Point of Sales ticket logo
+    odoo Point of Sale Customize Point of Sale Receipts Point of Sales Receipt Template custom Point of Sales order Edit/Modify Point of Sale Receipt Edit/Modify
+
+
+
+
+
+    """,
+    "website" : "www.browseinfo.in",
+    "data": [
+        'views/custom_pos_view.xml',
+    ],
+    'qweb': ['static/src/xml/pos_logo_change.xml',],
+    "auto_install": False,
+    "installable": True,
+    "live_test_url":'https://youtu.be/RLEkmLUs2MY',
+    "images":['static/description/Banner.png'],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
